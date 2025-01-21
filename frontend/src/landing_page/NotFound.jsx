d613769d3ca6900c6e329e0359a35ea88b4c66c2
Huvez8113 <huvez8113@gmail.com>
2025-01-21 12:51:39 +0530

    added class in notfound page

--- frontend/src/landing_page/NotFound.jsx
+++ frontend/src/landing_page/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
         <>
            <div className="container p-5 my-5">
                 <div className="row text-center">
-                    <h1 className="mt-5">404 Not Found</h1>
+                    <h1 className="mt-5 fs-2">404 Not Found</h1>
                     <p>Sorry, The page you are looking for does not exist</p>
                     <NavLink className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin : "0 auto"}} to="/">Go to Homepage</NavLink>
                 </div>
