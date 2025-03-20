2bd3a58a9560216313f2850c61524078a580eb1c
Huvez8113 <huvez8113@gmail.com>
2025-03-20 15:45:42 +0530

    Success

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -11,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<Home/>}/>
-          <Route path="/dashboard/" element={<Dashboard/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
